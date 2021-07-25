d1cfc98f97e6419b6ebadbd3786f70b8a938c30c
Hanna Hryhorieva <74866014+HannaHryhorieva@users.noreply.github.com>
2021-07-26 01:05:49 +0300

    Подгружает изображения без стилей

--- src/js/components/rendering-movies.js
+++ src/js/components/rendering-movies.js
@@ -1,11 +1,15 @@
 import * as moviesDBApi from '../api/moviesdb-api';
 // import getGenresByIds from '../api/genres-library';
 import genres from '../../data/genres.json';
+import cardMrc from '../../templates/one-card-mrc.hbs'
+const BASE_URL = 'http://image.tmdb.org/t/p/';
+const BASE_WIDTH = 'w500';
 
-moviesDBApi.getMoviesByQuery('cat', 2).then(console.log);
-moviesDBApi.getMovieById(400).then(console.log);
+// moviesDBApi.getMoviesByQuery('cat', 2).then(console.log);
+// moviesDBApi.getMovieById(400).then(console.log);
+moviesDBApi.getTrendingMovies().then(makeArrayForRendering).then(renderingGalerry);
 
-moviesDBApi.getTrendingMovies().then(makeArrayForRendering);
+const filmList = document.querySelector('.films__list');
 
 const getGenresByIds = function (genreIds) {
   if (!Array.isArray(genreIds)) return [];
@@ -14,14 +18,15 @@ const getGenresByIds = function (genreIds) {
 
 function makeArrayForRendering(data) {
     const arrMovies = data.results;
-    // const id = getGenresByIds(arrMovies[1].genre_ids);
     const arrayForRendering = arrMovies.map(movie => {
         const newArrayId = getGenresByIds(movie.genre_ids);
         const arrStrName = arrGengies(newArrayId);
         movie.stringGenries = makeStringGenries(arrStrName);
+        movie.poster_url = makeUrl(movie.poster_path)
+
         return movie;
-    })
-    return console.log(arrayForRendering);
+    });
+    return arrayForRendering;
 };
 function arrGengies (array) {
     return array.map(item => item.name)
@@ -32,4 +37,12 @@ function makeStringGenries (arrStrName) {
      return `${arrStrName[0]}, ${arrStrName[1]}, Other|`   
     };
     return arrStrName.join(',')
+};
+function makeUrl(partialURL) {
+    return `${BASE_URL}${BASE_WIDTH}${partialURL}`
 }
+
+function renderingGalerry(arrayForRendering) {
+    const galleryMrc = cardMrc(arrayForRendering);
+    filmList.insertAdjacentHTML('beforeend', galleryMrc)
+};
