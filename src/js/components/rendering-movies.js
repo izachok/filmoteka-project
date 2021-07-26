d06d27b92860ce4d927127ee77561d1e40bee058
Hanna Hryhorieva <74866014+HannaHryhorieva@users.noreply.github.com>
2021-07-26 12:20:17 +0300

    One-card and function rendering

--- src/js/components/rendering-movies.js
+++ src/js/components/rendering-movies.js
@@ -1,13 +1,14 @@
 import * as moviesDBApi from '../api/moviesdb-api';
 // import getGenresByIds from '../api/genres-library';
 import genres from '../../data/genres.json';
-import cardMrc from '../../templates/one-card-mrc.hbs'
+import cardMrc from '../../templates/one-card-mrc.hbs';
+
 const BASE_URL = 'http://image.tmdb.org/t/p/';
 const BASE_WIDTH = 'w342';
 
 // moviesDBApi.getMoviesByQuery('cat', 2).then(console.log);
 // moviesDBApi.getMovieById(400).then(console.log);
-moviesDBApi.getTrendingMovies().then(makeArrayForRendering).then(renderingGalerry);
+moviesDBApi.getTrendingMovies().then(makeArrayToRendering).then(renderingGalerry);
 
 const filmList = document.querySelector('.films__list');
 
@@ -16,34 +17,40 @@ const getGenresByIds = function (genreIds) {
   return genres.filter(genre => genreIds.includes(genre.id));
 };
 
-function makeArrayForRendering(data) {
+function makeArrayToRendering(data) {
     const arrMovies = data.results;
-    const arrayForRendering = arrMovies.map(movie => {
+    const arrayToRendering = arrMovies.map(movie => {
         const newArrayId = getGenresByIds(movie.genre_ids);
         const arrStrName = arrGengies(newArrayId);
         movie.stringGenries = makeStringGenries(arrStrName);
-        movie.poster_url = makeUrl(movie.poster_path)
-
+        movie.poster_url = makeUrl(movie.poster_path);
+        movie.release_year = makeYear(movie.release_date);
         return movie;
     });
-    return arrayForRendering;
+    return arrayToRendering;
 };
+
 function arrGengies (array) {
     return array.map(item => item.name)
 };
+
 function makeStringGenries (arrStrName) {
     if (!Array.isArray(arrStrName)) return '';
     if (arrStrName.length > 2) {
-     return `${arrStrName[0]}, ${arrStrName[1]}, Other|`   
+     return `${arrStrName[0]}, ${arrStrName[1]}, Other |`   
     };
-    return arrStrName.join(',')
+    return arrStrName.join(',').concat(' |')
 };
 
 function makeUrl(partialURL) {
     return `${BASE_URL}${BASE_WIDTH}${partialURL}`
-}
+};
 
-function renderingGalerry(arrayForRendering) {
-    const galleryMrc = cardMrc(arrayForRendering);
+function makeYear(dateStr) {
+    return dateStr.slice(0, 4)
+};
+
+function renderingGalerry(arrayToRendering) {
+    const galleryMrc = cardMrc(arrayToRendering);
     filmList.insertAdjacentHTML('beforeend', galleryMrc)
 };
