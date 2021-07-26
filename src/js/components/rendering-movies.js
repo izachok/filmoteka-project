c89b4ad8fd29dce2ef85b8fb34230c326dd0a468
Hanna Hryhorieva <74866014+HannaHryhorieva@users.noreply.github.com>
2021-07-26 15:41:23 +0300

    Bug fixes

--- src/js/components/rendering-movies.js
+++ src/js/components/rendering-movies.js
@@ -1,20 +1,20 @@
 
 import * as moviesDBApi from '../api/moviesdb-api'; //todo: delete after checking
 import { getGenresByIds } from '../api/genres-library';
-import cardMrc from '../../templates/one-card-mrc.hbs';
+import cardMarkup from '../../templates/one-card-markup.hbs';
 
 const BASE_URL = 'http://image.tmdb.org/t/p/';
 const BASE_WIDTH = 'w342';
 
-moviesDBApi.getTrendingMovies().then(makeArrayToRendering).then(arr => renderingGalerry(arr, filmList)); //todo: delete after checking
+moviesDBApi.getTrendingMovies().then(makeMoviesArrayForRendering).then(arr => renderGallery(arr, filmList)); //todo: delete after checking
 
 const filmList = document.querySelector('.films__list'); //todo: delete after checking
 
-function arrGengies (array) {
+function arrGenges (array) {
     return array.map(item => item.name)
 };
 
-function makeStringGenries (arrStrName) {
+function makeStringGenres (arrStrName) {
     if (!Array.isArray(arrStrName)) return '';
     if (arrStrName.length > 2) {
      return `${arrStrName[0]}, ${arrStrName[1]}, Other |`   
@@ -30,22 +30,22 @@ function makeYear(dateStr) {
     return dateStr.slice(0, 4)
 };
 
-function makeArrayToRendering(data) {
+function makeMoviesArrayForRendering(data) {
     const arrMovies = data.results;
-    const arrayToRendering = arrMovies.map(movie => {
+    const arrayForRendering = arrMovies.map(movie => {
         const newArrayId = getGenresByIds(movie.genre_ids);
-        const arrStrName = arrGengies(newArrayId);
-        movie.stringGenries = makeStringGenries(arrStrName);
-        movie.poster_url = makeUrl(movie.poster_path);
-        movie.release_year = makeYear(movie.release_date);
+        const arrStrName = arrGenges(newArrayId);
+        movie.stringGenres = makeStringGenres(arrStrName);
+        movie.posterUrl = makeUrl(movie.poster_path);
+        movie.releaseYear = makeYear(movie.release_date);
         return movie;
     });
-    return arrayToRendering;
+    return arrayForRendering;
 };
 
-function renderingGalerry(arrayToRendering, element) {
-    const galleryMrc = cardMrc(arrayToRendering);
-    element.insertAdjacentHTML('beforeend', galleryMrc)
+function renderGallery(arrayForRendering, element) {
+    const galleryMarkup = cardMarkup(arrayForRendering);
+    element.insertAdjacentHTML('beforeend', galleryMarkup)
 };
 
-export { makeArrayToRendering, renderingGalerry }
+export { makeMoviesArrayForRendering, renderGallery }
