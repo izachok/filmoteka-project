c64bf21612fe8badc6377382ee0675e6d30ff839
Aleksandra605 <72212649+Aleksandra605@users.noreply.github.com>
2021-07-28 18:29:08 +0200

    nevermind

--- src/js/index.js
+++ src/js/index.js
@@ -3,12 +3,13 @@ import '../sass/main.scss';
 import * as footerModal from './components/footer-modal';
 import { pageState } from './components/pageState';
 import { initSearch } from './components/search';
-import { renderTopMovies } from './components/rendering-top-movies'
+import { renderTopMovies } from './components/rendering-top-movies';
+import { handleFetch, showModal } from './components/modal-movie';
 //example of using api functions
 // import * as moviesDBApi from './api/moviesdb-api';
 // moviesDBApi.getTrendingMovies().then(console.log);
 // moviesDBApi.getMoviesByQuery('cat', 2).then(console.log);
-// moviesDBApi.getMovieById(400).then(console.log);
+moviesDBApi.getMovieById(400).then(console.log);
 
 import { makeMoviesArrayForRendering, renderGallery } from './components/rendering-movies';
 
@@ -17,7 +18,5 @@ initSearch();
 renderTopMovies();
 // example of using makeMoviesArrayForRendering, renderGallery functions
 // moviesDBApi.getTrendingMovies().then(makeMoviesArrayForRendering).then(arr => renderGallery(arr, filmList));
-
-
-
-
+handleFetch();
+showModal();
