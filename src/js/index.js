02709181723e11fdd42ea5c4bf0382432c1ee2bd
Aleksandra605 <72212649+Aleksandra605@users.noreply.github.com>
2021-07-30 09:00:56 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -7,15 +7,17 @@ import { renderApp } from './components/renderer';
 import { initNavigation } from './components/navigation';
 
 import { renderTopMovies } from './components/rendering-top-movies';
-import { handleFetch, showModal } from './components/modal-movie';
 import * as localDB from './components/localDB';
 import './components/to-top-btn';
-import { onClickWatchedBtn , onClickQueueBtn } from './components/functionality-watched-queue-button';
+import {
+  onClickWatchedBtn,
+  onClickQueueBtn,
+} from './components/functionality-watched-queue-button';
 //example of using api functions
 import * as moviesDBApi from './api/moviesdb-api';
 // moviesDBApi.getTrendingMovies().then(console.log);
 // moviesDBApi.getMoviesByQuery('cat', 2).then(console.log);
-moviesDBApi.getMovieById(400).then(console.log);
+// moviesDBApi.getMovieById(400).then(console.log);
 
 import { makeMoviesArrayForRendering, renderGallery } from './components/rendering-movies';
 
@@ -28,8 +30,6 @@ renderTopMovies();
 
 // example of using makeMoviesArrayForRendering, renderGallery functions
 // moviesDBApi.getTrendingMovies().then(makeMoviesArrayForRendering).then(arr => renderGallery(arr, filmList));
-handleFetch();
-showModal();
 
 // example of using localStorage
 // import * as localDB from './components/localDB';
