e8e5277c646f45a84e88a03754139bf6829a0602
Tatiana Skalska <tatiana.skalska@gmail.com>
2021-08-05 13:34:08 +0200

    notiflix color change

--- src/js/components/to-trash-btn.js
+++ src/js/components/to-trash-btn.js
@@ -23,10 +23,18 @@ function initBtnToClear() {
   pageContainerEl.insertAdjacentHTML('beforeend', btnToEmpty());
   const buttonClearLibrary = document.querySelector('.button_clear-library');
   buttonClearLibrary.addEventListener('click', () => {
-    Confirm.show('Delete library list', 'Are you sure?', 'Yes', 'No', function () {
-      clearLibrary(pageState);
-    });
-  });
+    Confirm.show(
+      'Delete library list', 
+      'Are you sure?', 
+      'Yes', 
+      'No', 
+      function () {
+        clearLibrary(pageState)
+      }, 
+      null,
+      { titleColor:"#ff6b01", okButtonBackground:"#ff6b01"}
+    )
+  })
 }
 
 function clearLibrary() {
